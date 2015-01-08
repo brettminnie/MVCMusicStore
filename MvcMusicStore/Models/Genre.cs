9f6518f0380417c802d61c1e9d0949603630588c
Brett Minnie <github@bdb-studios.co.uk>
2015-01-08 16:55:56 +0000

    Validation and Display Name annotations

--- MvcMusicStore/Models/Genre.cs
+++ MvcMusicStore/Models/Genre.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MvcMusicStore.Models
 {
     public partial class Genre
     {
         public int GenreId { get; set; }
+
+        [DisplayName("Genre")]
         public string Name { get; set; }
+        
         public string Description { get; set; }
+
         public List<Album> Albums { get; set; }
     }
 }
