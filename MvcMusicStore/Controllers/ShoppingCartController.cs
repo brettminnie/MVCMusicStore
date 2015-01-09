ee9c991af8d3fba396f9a8d8a1e1122d067035b5
Brett Minnie <github@bdb-studios.co.uk>
2015-01-09 10:56:37 +0000

    Added shopping cart controller

---
+++ MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+
+        MusicStoreEntities storeDb = new MusicStoreEntities();
+
+        //
+        // GET: /ShoppingCart/
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            var viewModel = new ShoppingCartViewModel {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            return View(viewModel);
+        }
+
+        //
+        //GET : /Store/AddToCart/5
+        public ActionResult AddToCart(int id)
+        {
+            var addedAlbum = storeDb.Albums
+                .Single(album => album.AlbumId == id);
+
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedAlbum);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
